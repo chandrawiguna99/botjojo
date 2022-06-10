c918b8ba8686d923512ccd725ec205574d1eb0d3
chandrawiguna99 <chndrwgnn66@gmail.com>
2022-06-10 19:08:21 +0700

    botjojo

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+apt-get install imagemagick git nodejs ffmpeg libwebp mc nano
+git clone https://github.com/ZeeoneOfficial/node_modules
+
+
+echo "All dependencies have been installed, please run the command \"npm start\" to immediately start the script"
